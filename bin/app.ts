498d712c79dc027b5cfeb579b952d22c8d6a3b56
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-11-06 18:57:46 +0530

    Update app.ts

--- bin/app.ts
+++ bin/app.ts
@@ -4,6 +4,9 @@ import { StreamlinkStack } from '../lib/streamlink-stack';
 
 const app = new cdk.App();
 new StreamlinkStack(app, 'StreamlinkStack', {
-  env: { region: 'eu-north-1' },
+  // region/account will come from the ENV in GitHub Actions
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
-
